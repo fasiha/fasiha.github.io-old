069d7b1a446b658472217eb4bf32bd703196e4e5
Ahmed Fasih <wuzzyview@gmail.com>
2015-12-16 22:44:29 -0500

    Tentative update

--- index.html.pm
+++ index.html.pm
@@ -1,5 +1,7 @@
 #lang pollen
 
+◊(define japan-url "http://maps.aldebrn.me/Apps/#%7B%22version%22%3A0%2C%22baseLayerPicked%22%3A%7B%22name%22%3A%22The%20Black%20Marble%22%2C%22iconUrl%22%3A%22http%3A%2F%2Fmaps.aldebrn.me%2FBuild%2FCesium%2FWidgets%2FImages%2FImageryProviders%2FblackMarble.png%22%7D%2C%22layers%22%3A%5B%7B%22url%22%3A%22%2F%2Fcesiumjs.org%2Fblackmarble%22%2C%22isBaseLayer%22%3Atrue%2C%22alpha%22%3A1%2C%22brightness%22%3A1%2C%22contrast%22%3A1%2C%22hue%22%3A0%2C%22saturation%22%3A1%2C%22gamma%22%3A0.48%7D%2C%7B%22url%22%3A%22http%3A%2F%2Fmaps.aldebrn.me%2Fworld-tex-cgiar-250m%22%2C%22isBaseLayer%22%3Afalse%2C%22alpha%22%3A0.52%2C%22brightness%22%3A1.32%2C%22contrast%22%3A1.4%2C%22hue%22%3A0%2C%22saturation%22%3A1%2C%22gamma%22%3A1%7D%5D%2C%22destination%22%3A%7B%22x%22%3A-4555669.674111755%2C%22y%22%3A3702876.47327235%2C%22z%22%3A3697615.9732322954%7D%2C%22orientation%22%3A%7B%22heading%22%3A5.681369036822533%2C%22pitch%22%3A-0.8218479123823923%2C%22roll%22%3A0.027079069039927184%7D%7D")
+
 ◊title{Aldebrn.me・◊em{code & math, languages & maps}}
 ◊author{Ahmed Fasih}
 
@@ -7,7 +9,9 @@
 
 ◊h2{Projects}
 
-◊marginnote["fig-japan"]{◊img['src: "texshade/images/permalink-screenshot.png"]}
+I’ve only recently prioritized the “finishing”◊sidenote["fogus-finishing"]{See the pellucid Michael Fogus’ ◊link["http://blog.fogus.me/2015/11/04/the-100101-method-my-approach-to-open-source/"]{“The 100:10:1 method”}} of projects. Snapshots of micro-projects in various states of undress are posted on ◊link["https://twitter.com/fasihsignal"]{Twitter} and, for the brave, ◊link["https://github.com/fasiha"]{GitHub}.
+
+◊marginnote["fig-japan"]{◊a['href: japan-url]{◊img['src:"texshade/images/permalink-screenshot-small.jpg"]}}
 ◊h3{Cartography}
 ◊link["texshade/"]{Texture-shaded terrain globes in your browser}
 
--- texshade/index.html.pm
+++ texshade/index.html.pm
@@ -8,9 +8,9 @@
 Welcome to my ◊a['href:"http://cesiumjs.org"]{Cesium.js} cartographic showcase. Please visit one of the following applications.
 
 ◊marginnote["fig-japan"]{◊img['src: "images/permalink-screenshot.png"]}
-◊h2{◊a['href: "Apps/"]{Texture-shaded terrain visualization} (◊a['href: "texshade.html"]{tutorial})}
+◊h2{◊a['href: "http://maps.aldebrn.me/Apps/"]{Texture-shaded terrain visualization} (◊a['href: "texshade.html"]{tutorial})}
 
-The ◊a['href: "texshade.html"]{tutorial} will help you get started if you’ve never used Cesium before.
+The ◊a['href: "tutorial.html"]{tutorial} will help you get started if you’ve never used Cesium before.
 
 ◊h3{◊a['href: "http://maps.aldebrn.me/Apps/#%7B%22version%22%3A0%2C%22baseLayerPicked%22%3A%7B%22name%22%3A%22The%20Black%20Marble%22%2C%22iconUrl%22%3A%22http%3A%2F%2Fmaps.aldebrn.me%2FBuild%2FCesium%2FWidgets%2FImages%2FImageryProviders%2FblackMarble.png%22%7D%2C%22layers%22%3A%5B%7B%22url%22%3A%22%2F%2Fcesiumjs.org%2Fblackmarble%22%2C%22isBaseLayer%22%3Atrue%2C%22alpha%22%3A1%2C%22brightness%22%3A1%2C%22contrast%22%3A1%2C%22hue%22%3A0%2C%22saturation%22%3A1%2C%22gamma%22%3A0.48%7D%2C%7B%22url%22%3A%22http%3A%2F%2Fmaps.aldebrn.me%2Fworld-tex-cgiar-250m%22%2C%22isBaseLayer%22%3Afalse%2C%22alpha%22%3A0.52%2C%22brightness%22%3A1.32%2C%22contrast%22%3A1.4%2C%22hue%22%3A0%2C%22saturation%22%3A1%2C%22gamma%22%3A1%7D%5D%2C%22destination%22%3A%7B%22x%22%3A-4555669.674111755%2C%22y%22%3A3702876.47327235%2C%22z%22%3A3697615.9732322954%7D%2C%22orientation%22%3A%7B%22heading%22%3A5.681369036822533%2C%22pitch%22%3A-0.8218479123823923%2C%22roll%22%3A0.027079069039927184%7D%7D"]{Japan: Mountains and Light. 日本、山と光}}
 
